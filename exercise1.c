f8ac8c693f131303e1b3636364872d74a49d0f12
aqillakhamis <aqillakhamis@gmail.com>
2022-05-15 20:51:46 +0800

    update tutorial week 6

---
+++ exercise1.c
@@ -0,0 +1,17 @@
+//documentation section 
+/* Exercise 1 - Bubble Sort*/
+
+//pre-processor section 
+#include<stdio.h>
+
+//global variable section 
+
+
+//main function section 
+int main(){
+    //write here your program
+    
+
+}
+
+//user-defined section 
